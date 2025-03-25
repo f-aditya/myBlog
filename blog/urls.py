99a5b595d84ea7e19995b3c9c33db53c1ea8e16e
faridlinein <farid.aditya@gmail.com>
2025-03-26 00:25:25 +0700

    send email

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "blog"
+
 urlpatterns = [
     # post views
     # path("", views.post_list, name="post_list"),
@@ -12,4 +13,5 @@ urlpatterns = [
         views.post_detail,
         name="post_detail",
     ),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
 from .forms import EmailPostForm
 
@@ -55,13 +56,33 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False  # Status pengiriman email
     if request.method == "POST":
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            # Buat subject dan isi email
+            subject = (
+                f"{cd["name"]} ({cd["email"]})" f"recommends you read {post.title}"
+            )
+            message = (
+                f"read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=none,
+                recipient_list=[cd["to"]],
+            )
+            sent = True  # Tandai email berhasil dikirim
+
     else:
         form = EmailPostForm()
-    return render(request, "blog/post/share.html", {"post": post, "form": form})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
